ba6fdcb48e3a996d0b2905de31a5b5e5059eb1a3
klozz <carlosj@klozz.dev>
2022-07-04 10:08:29 +0200

    drop megvi shit  and dont build libperfmgr from here until defined
    Signed-off-by: klozz <carlosj@klozz.dev>

--- aidl/power-libperfmgr/Android.mk
+++ aidl/power-libperfmgr/Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
 ifneq (,$(findstring hardware/google/interfaces, $(PRODUCT_SOONG_NAMESPACES)))
 ifneq (,$(findstring hardware/google/pixel, $(PRODUCT_SOONG_NAMESPACES)))
 
@@ -62,5 +62,6 @@ LOCAL_VINTF_FRAGMENTS := android.hardware.power-service.xiaomi.xml
 
 include $(BUILD_EXECUTABLE)
 
+endif
 endif
 endif
